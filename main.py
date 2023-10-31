9ddc1de64daeb472a60fba7429783e61311bf87a
Adrian <reinieradrianluna@gmail.com>
2023-10-31 16:52:38 +0800

    Adding Images and AsyncImage

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.image import Image, AsyncImage
 
 
 class MainApp(App):
     def build(self):
-        button = Button(text='Print this', size_hint=(0.2,0.2), font_size = '20sp',
-                        pos_hint={'center_x':0.5, 'center_y':0.5},
-                        on_press=self.printpress,
-                        on_release=self.printrelease
-                        )
-        return button
-    
-    def printpress(self,obj):
-        print("Button has been pressed")
-        
-    def printrelease(self,obj):
-        print("Button has been released")
+        # img = Image(source='sdasd.png')
+        img = AsyncImage(source='https://static.vecteezy.com/system/resources/previews/024/029/752/original/house-icon-clipart-transparent-background-free-png.png')
+        return img
         
         
     
