ef7a72b3daa5e0e88eef0aa18d98ea463f5fe135
Adrian <reinieradrianluna@gmail.com>
2023-10-31 16:46:25 +0800

    Creating Buttons and Background Color

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from kivy.app import App
-from kivy.uix.label import Label 
-from kivy.core.window import Window
+from kivy.uix.button import Button
 
-Window.clearcolor = (1,1,1,1)
 
 class MainApp(App):
     def build(self):
-        label = Label(text='This is Batman.', font_size='20sp', bold = True, 
-                      color = (0,0,0,1),
-                      italic = True)
-        return label
+        button = Button(text='Print this', size_hint=(0.2,0.2), font_size = '20sp',
+                        pos_hint={'center_x':0.5, 'center_y':0.5},
+                        on_press=self.printpress,
+                        on_release=self.printrelease
+                        )
+        return button
+    
+    def printpress(self,obj):
+        print("Button has been pressed")
+        
+    def printrelease(self,obj):
+        print("Button has been released")
+        
+        
     
 MainApp().run()
