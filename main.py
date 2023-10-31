5512089b8cc395977e52a20cc0f84be4a44f3aa7
Adrian <reinieradrianluna@gmail.com>
2023-10-31 17:14:07 +0800

    addding a button w/ an image using box layout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 
@@ -8,13 +9,11 @@ Window.size = (360,600)
 
 class MainApp(App):
     def build(self):
-       layout = BoxLayout(orientation='vertical', spacing=10, padding=40)
-       btn = Button(text='Button1')
-       btn2 = Button(text='Button2')
-       btn3 = Button(text='Button3')
-       layout.add_widget(btn)
-       layout.add_widget(btn2)
-       layout.add_widget(btn3)
+       layout = BoxLayout(orientation='vertical', spacing = 150, padding = 80)
+       img = Image(source = 'bahay.png')
+       button = Button(text = 'Login', size_hint = (None,None), width= 100, height = 50, pos_hint={'center_x': 0.5})
+       layout.add_widget(img)
+       layout.add_widget(button)
        return layout
         
 MainApp().run()
